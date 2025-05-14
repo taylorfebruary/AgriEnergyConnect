15b34341bb71b9be6f5993c0ab83e429258e810f
Taylor February <ST10037533@vcconnect.edu.za>
2025-05-14 20:06:39 +0200

    Product view and edit FarmerController

--- AgriEnergyConnect/Controllers/FarmerController.cs
+++ AgriEnergyConnect/Controllers/FarmerController.cs
@@ -5,45 +5,49 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-[Authorize(Roles = "Farmer")]
-public class FarmerController : Controller
+namespace AgriEnergyConnect
 {
-    private readonly ApplicationDbContext _context;
-    private readonly UserManager<ApplicationUser> _userManager;
 
-    public FarmerController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    [Authorize(Roles = "Farmer")]
+    public class FarmerController : Controller
     {
-        _context = context;
-        _userManager = userManager;
-    }
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-    public async Task<IActionResult> MyProducts()
-    {
-        var user = await _userManager.GetUserAsync(User);
-        var farmer = await _context.Farmers.FirstOrDefaultAsync(f => f.Email == user.Email);
+        public FarmerController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
 
-        var products = await _context.Products
-            .Where(p => p.FarmerId == farmer.Id)
-            .ToListAsync();
-
-        return View(products);
-    }
-
-    public IActionResult AddProduct() => View();
-
-    [HttpPost]
-    public async Task<IActionResult> AddProduct(Product product)
-    {
-        if (ModelState.IsValid)
+        public async Task<IActionResult> MyProducts()
         {
             var user = await _userManager.GetUserAsync(User);
-            var farmer = await _context.Farmers.FirstOrDefaultAsync(f => f.Email == user.Email);
+            var products = await _context.Products
+                .Where(p => p.FarmerId == user.Id)
+                .ToListAsync();
 
-            product.FarmerId = farmer.Id;
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("MyProducts");
+            return View(products);
         }
-        return View(product);
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                product.FarmerId = user.Id;
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("MyProducts");
+            }
+
+            return View(product);
+        }
     }
 }
